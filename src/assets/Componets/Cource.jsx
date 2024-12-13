0c2cf866b7272d323043a948fc056f4424eb5760
Shubham Singh Prajapati <siddharthsingh75250@gmail.com>
2024-12-13 12:18:47 +0530

    Code Update

--- src/assets/Componets/Cource.jsx
+++ src/assets/Componets/Cource.jsx
@@ -1,12 +1,35 @@
-
+import { FaStar } from "react-icons/fa";
 
 const Cource = () => {
   return (
-    <div className="flex content-center ">
-      <div className="left "><h1>Popular Courses</h1></div>
-      <div className="right"></div>
+    <div className=" h-[80vh]">
+      <div className="">
+        <h1 className="text-5xl font-bold">Popular Courses</h1>
+      </div>
+      <div className="right flex gap-12 mt-11 ">
+        <div className="box1 bg-white border-2 border-black rounded-xl p-4">
+          {" "}
+          <img src="/public/images/164661.jpg" alt=""  className="w-[17vw] rounded-xl "/> <h3 className="text-xl font-bold mt-4">Digital Marketing & With <br/>
+          Basics to Advance</h3> <p className="mt-6">Clifford Lampe</p> <h4 className="mt-3 mb-3 font-bold text-[1.25rem] flex items-center gap-2">5.0<FaStar /><FaStar /><FaStar /><FaStar /><FaStar /></h4> <h3 className="text-[1.55rem] font-bold">$19.60</h3>
+        </div>
+        <div className="box1 bg-white border-2 border-black rounded-xl p-4">
+          {" "}
+          <img src="/public/images/164661.jpg" alt=""  className="w-[17vw] rounded-xl "/> <h3 className="text-xl font-bold mt-4">Digital Marketing & With <br/>
+          Basics to Advance</h3> <p className="mt-6">Clifford Lampe</p> <h4 className="mt-3 mb-3 font-bold text-[1.25rem] flex items-center gap-2">5.0<FaStar /><FaStar /><FaStar /><FaStar /><FaStar /></h4> <h3 className="text-[1.55rem] font-bold">$19.60</h3>
+        </div>
+        <div className="box1 bg-white border-2 border-black rounded-xl p-4">
+          {" "}
+          <img src="/public/images/164661.jpg" alt=""  className="w-[17vw] rounded-xl "/> <h3 className="text-xl font-bold mt-4">Digital Marketing & With <br/>
+          Basics to Advance</h3> <p className="mt-6">Clifford Lampe</p> <h4 className="mt-3 mb-3 font-bold text-[1.25rem] flex items-center gap-2">5.0<FaStar /><FaStar /><FaStar /><FaStar /><FaStar /></h4> <h3 className="text-[1.55rem] font-bold">$19.60</h3>
+        </div>
+        <div className="box1 bg-white border-2 border-black rounded-xl p-4">
+          {" "}
+          <img src="/public/images/164661.jpg" alt=""  className="w-[17vw] rounded-xl "/> <h3 className="text-xl font-bold mt-4">Digital Marketing & With <br/>
+          Basics to Advance</h3> <p className="mt-6">Clifford Lampe</p> <h4 className="mt-3 mb-3 font-bold text-[1.25rem] flex items-center gap-2">5.0<FaStar /><FaStar /><FaStar /><FaStar /><FaStar /></h4> <h3 className="text-[1.55rem] font-bold">$19.60</h3>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cource
+export default Cource;
